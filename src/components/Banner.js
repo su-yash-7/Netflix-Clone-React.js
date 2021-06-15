3ba442f92a035124a7ef28c5e9e9e37341a8b681
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-15 23:40:30 +0530

    Implementing the API

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -10,21 +26,15 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner_content">
-        <h1 className="banner_title">Movie Name</h1>
-        <h1 className="banner_description">
-          {truncate(
-            `Test Description Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Modi non enim veniam iure ipsam, velit nostrum magnam, deserunt
-          libero saepe dolor eum. Eligendi, ipsa quidem explicabo voluptas
-          aliquid magnam labore.`,
-            150
-          )}
+        <h1 className="banner_title">
+          {movie?.name || movie?.title || movie?.orginal_name}
         </h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">More Info</button>
