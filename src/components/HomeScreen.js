b33048692811752bb6d889cb88dba220c1abdbb6
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-16 22:55:22 +0530

    Building The Rows

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import styled from "styled-components";
+import requests from "./Requests";
 import Banner from "./Banner";
 import Nav from "./Nav";
+import Row from "./Row";
 function HomeScreen() {
   return (
     <homeScreen>
       <Nav />
       <Banner />
+      <Row
+        isLargeRow
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </homeScreen>
   );
 }
-const homeScreen = styled.div``;
 export default HomeScreen;
