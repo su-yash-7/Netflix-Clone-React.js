4325bf8a33b781b029ae5e0f60e7b29d0ff4ab48
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-18 14:35:13 +0530

    Building Login Screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { React, useState } from "react";
 import "./styles/LoginScreen.css";
+import SignupScreen from "./SignupScreen";
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
@@ -9,13 +12,36 @@ function LoginScreen() {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button className="loginScreen_button">Sign In</button>
+        <button onClick={() => setSignIn(true)} className="loginScreen_button">
+          Sign In
+        </button>
         <div className="loginScreen_gradient"></div>
       </div>
       <div className="loginScreen_body">
-        <>
-          <h1>Unlimited movies, TV shows and more.</h1>
-        </>
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+          <>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen_getStarted"
+                >
+                  {" "}
+                  Get Started
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignupScreen() {
+  return <div></div>;
+}
+
+export default SignupScreen;
