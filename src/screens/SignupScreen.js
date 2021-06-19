416473a0d7e14ec0061205dfd3c5b5a28729630e
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-19 10:36:36 +0530

    Building SignIn/Up Screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import "./styles/SignupScreen.css";
 function SignupScreen() {
-  return <div></div>;
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email Address" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="signupScreen_gray">New to Netflix?</span>{" "}
+          <span className="signupScreen__link">Sign up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default SignupScreen;
